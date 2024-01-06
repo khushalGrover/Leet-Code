a074d69e3ecd700242f0243e7b5c4c0ce18c46e9
khushalGrover <khushal15Grover@gmail.com>
2024-01-07 00:35:37 +0530

    checkpoint some more text changed

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -8,13 +8,22 @@ import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
 const Hero = () => (
-  <Background color="bg-gray-100">
+  <Background color="bg-red-100">
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
+          <Link href="/">
+            <span className="text-primary-500">Premium</span>
           </Link>
+        </li>
+        <li>
+          <Link href="/">Explore</Link>
+        </li>
+        <li>
+          <Link href="/">Product</Link>
+        </li>
+        <li>
+          <Link href="/">Developer</Link>
         </li>
         <li>
           <Link href="/">Sign in</Link>
@@ -26,14 +35,14 @@ const Hero = () => (
       <HeroOneButton
         title={
           <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
+            {'A New Way to '}
+            <span className="text-primary-500">Learn</span>
           </>
         }
-        description="The easiest way to build a React landing page in seconds."
+        description="LeetCode is the best platform to help you enhance your skills, expand your knowledge and prepare for technical interviews."
         button={
           <Link href="https://creativedesignsguru.com/category/nextjs/">
-            <Button xl>Download Your Free Theme</Button>
+            <Button xl>Create Account</Button>
           </Link>
         }
       />
--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -2,26 +2,23 @@ import { VerticalFeatureRow } from '../feature/VerticalFeatureRow';
 import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
-  <Section
-    title="Your title here"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
-  >
+  <Section title="" description="">
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      title="Start Exploring"
+      description="Explore is a well-organized tool that helps you get the most out of LeetCode by providing structure to guide your progress towards the next step in your programming career."
       image="/assets/images/feature.svg"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      title="Questions, Community & Contests"
+      description="Over 3150 questions for you to practice. Come and join one of the largest tech communities with hundreds of thousands of active users and participate in our contests to challenge yourself and earn rewards."
       image="/assets/images/feature2.svg"
       imageAlt="Second feature alt text"
       reverse
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      title="Companies & Candidates"
+      description="Not only does LeetCode prepare candidates for technical interviews, we also help companies identify top technical talent. From sponsoring contests to providing online assessments and training, we offer numerous services to businesses."
       image="/assets/images/feature3.svg"
       imageAlt="Third feature alt text"
     />
